273ea4ba3a829363791180c2f0e357c69e2012d5
malilasage <malilaclearwater@gmail.com>
2016-12-09 11:33:48 -0700

    added error functions

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
+
 //test route
 router.get('/users', (req, res, next) => {
   knex('users')
--- server.js
+++ server.js
@@ -4,14 +4,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const knex = require('./knex');
+const morgan = require('morgan');
+const path = require('path');
 
 app.disable('x-powered-by');
 
-app.use(express.static('public'));
+app.use(express.static(path.join('public')));
 
 //require routes
 const index = require('./routes/index');
 app.use(index);
+
+//error functions
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+  console.error(err.stack);
+  res.sendStatus(500);
+});
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
