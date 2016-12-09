6dde99279becb0c4c7525bbc9c087e13e88803e3
malilasage <malilaclearwater@gmail.com>
2016-12-09 11:04:02 -0700

    initial commit. basic get request works, accessing database through get request is not

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const knex = require('knex');
+
+app.disable('x-powered-by');
+
+app.use(express.static('public'));
+
+//require routes
+// const index = require('./routes/index');
+// app.use(index);
+
+app.get('/users', (req, res) => {
+  knex('users')
+  .orderBy('name')
+  .then((users) => {
+    res.send(users);
+  });
+});
+
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
+
+module.exports = app;
