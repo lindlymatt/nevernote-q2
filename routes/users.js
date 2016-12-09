6527db0d3d61c4e82da0d8fbfb4ef760faa7af8d
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-09 14:02:35 -0700

    Finished the users route PATCH with ID checking.

--- routes/users.js
+++ routes/users.js
@@ -49,5 +49,53 @@ router.get('/:id', ev(validations), (req, res, next) => {
 });
 
 // POST: Creates a new user.
+router.post('/', ev(validations), (req, res, next) => {
+  const {
+    firstName,
+    email,
+    password
+  } = req.body;
+
+  let newUser = { firstName, email };
+  bcrypt.hash(password, 10).then(result => {
+    newUser.password = result;
+  })
+  .then(() => {
+    knex('users')
+      .insert(decamelizeKeys(newUser));
+  })
+  .catch(err => {
+    next(err);
+  })
+});
+
+// PATCH w/ ID: Updates a user with the ID.
+router.patch('/:id', ev(validations), (req, res, next) => {
+  const {
+  firstName,
+  email,
+  password } = req.body;
+
+  let newUser = { firstName, email };
+  bcrypt.hash(password, 10).then(result => {
+    newUser.password = result;
+  }).then(() => {
+    // Query and make sure the ID is fine.
+    knex('users')
+      .max('id')
+      .then(results => {
+        if (!results || !id || id <= 0 || id > results) {
+          return res.status(401).send('Unauthorized Access.');
+        }
+        // After determining if the ID exists and is valid; find it.
+        knex('users')
+          .update({newUser})
+          .where('users.id', id);
+      })
+      .catch(err => {
+        next(err);
+      })
+  })
+});
 
 module.exports = router;
