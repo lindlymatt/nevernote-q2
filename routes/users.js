ee4b4832466acf8d6d296b8fa8b798cc1454239d
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-09 13:50:03 -0700

    Added the GET with ID route.

--- routes/users.js
+++ routes/users.js
@@ -11,20 +11,43 @@ var router = express.Router();
 var ev = require('express-validation');
 var validations = require('../validations/users');
 
-// GET
-router.get('/', ev(validations), (req, res, next) => {
+// GET: All users.
+router.get('/', (req, res, next) => {
   knex('users')
     .then(results => {
-      res.send(results);
+      res.status(200).send(results);
     })
     .catch(err => {
       next(err);
     })
 });
 
-// router.get('/:id', ev(validations), (req, res, next) => {
-//   knex('users')
-//     .
-// });
+// GET w/ ID: Gets a single user's information.
+router.get('/:id', ev(validations), (req, res, next) => {
+  const id = req.params.id;
+
+  knex('users')
+    .max('id')
+    .then(results => {
+      if (!id || id <= 0 || id > results) {
+        return res.status(401).send('Unauthorized Access.');
+      }
+      // After determining if the ID exists and is valid; find it.
+      knex('users')
+        .where('users.id', id)
+        .first()
+        .then(result => {
+          res.status(200).send(result);
+        })
+        .catch(err => {
+          next(err);
+        });
+    })
+    .catch(err => {
+      next(err);
+    })
+});
+
+// POST: Creates a new user.
 
 module.exports = router;
