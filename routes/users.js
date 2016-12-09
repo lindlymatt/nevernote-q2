56cebaa8fec932b008ed3407f533e1bcee4fde13
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-09 14:05:13 -0700

    Finished total USERS route with GET, POST, PATCH and DELETE.

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.get('/', (req, res, next) => {
     })
     .catch(err => {
       next(err);
-    })
+    });
 });
 
 // GET w/ ID: Gets a single user's information.
@@ -45,7 +45,7 @@ router.get('/:id', ev(validations), (req, res, next) => {
     })
     .catch(err => {
       next(err);
-    })
+    });
 });
 
 // POST: Creates a new user.
@@ -61,12 +61,12 @@ router.post('/', ev(validations), (req, res, next) => {
     newUser.password = result;
   })
   .then(() => {
-    knex('users')
+    return knex('users')
       .insert(decamelizeKeys(newUser));
   })
   .catch(err => {
     next(err);
-  })
+  });
 });
 
 // PATCH w/ ID: Updates a user with the ID.
@@ -88,14 +88,32 @@ router.patch('/:id', ev(validations), (req, res, next) => {
           return res.status(401).send('Unauthorized Access.');
         }
         // After determining if the ID exists and is valid; find it.
-        knex('users')
+        return knex('users')
           .update({newUser})
           .where('users.id', id);
       })
       .catch(err => {
         next(err);
-      })
-  })
+      });
+  });
+});
+
+// DELETE w/ ID: Deletes an individual user by ID.
+router.delete('/:id', ev(validations), (req, res, next) => {
+  knex('users')
+    .max('id')
+    .then(results => {
+      if (!results || !id || id <= 0 || id > results) {
+        return res.status(401).send('Unauthorized Access.');
+      }
+      // After determining if the ID exists delete it.
+      return knex('users')
+        .del()
+        .where('users.id', id);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
