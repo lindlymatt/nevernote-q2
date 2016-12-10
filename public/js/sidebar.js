27f289f513b787b358f093e39968482723b6ca09
Aidan Baack <aebaack@gmail.com>
2016-12-10 16:27:11 -0700

    Start testing structure of nested folders

---
+++ public/js/sidebar.js
@@ -0,0 +1,65 @@
+'use strict';
+
+function sidebar() {
+
+}
+
+let userInfo = {
+    "folders": [
+        {
+            "childFolders": [
+                {
+                    "childFolders": [],
+                    "folderNotes": [
+                        {
+                            "content": "This is Matt Pestridge's note \n ## This is the second line.",
+                            "created_at": "2016-12-10T20:28:40.197Z",
+                            "id": 2,
+                            "name": "Pestridge Test Note",
+                            "note_id": 3,
+                            "parent_folder": 3,
+                            "read_only": false,
+                            "updated_at": "2016-12-10T20:28:40.197Z",
+                            "user_id": 2
+                        }
+                    ],
+                    "id": 3,
+                    "is_secure": false,
+                    "name": "I gave my wife to Lindly",
+                    "parent_folder": 4,
+                    "user_id": 2
+                },
+                {
+                    "childFolders": [],
+                    "folderNotes": [],
+                    "id": 5,
+                    "is_secure": false,
+                    "name": "New Foldaa",
+                    "parent_folder": 4,
+                    "user_id": 2
+                }
+            ],
+            "folderNotes": [],
+            "id": 4,
+            "is_secure": false,
+            "name": "Lindly over Pestridge",
+            "parent_folder": null,
+            "user_id": 2
+        },
+        {
+            "childFolders": [],
+            "folderNotes": [],
+            "id": 2,
+            "is_secure": true,
+            "name": "Passwords Folder",
+            "parent_folder": null,
+            "user_id": 2
+        }
+    ],
+    "notes": [
+      {
+        id: 20,
+        name: 'My Random Note'
+      }
+    ]
+};
