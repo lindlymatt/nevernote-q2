cd6c633f2723fd28a665dbe8882199ebd394d9a1
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-10 16:15:21 -0700

    Changed some things.

--- routes/folders.js
+++ routes/folders.js
@@ -16,9 +16,11 @@ const { camelizeKeys, decamelizeKeys } = require('humps');
 
 // GET: All of the folders.
 router.get('/', (req, res, next) => {
+  // const uid = req.body.user_id;
   knex('folders')
+    .where('user_id', uid)
     .then(results => {
-      res.status(200).send(results);
+      res.send(results);
     })
     .catch(err => {
       next(err);
@@ -29,50 +31,40 @@ router.get('/', (req, res, next) => {
 router.get('/:id', ev(validations.get), (req, res, next) => {
   const id = req.params.id;
 
-  knex('users')
-    .max('id')
-    .then(results => {
-      if (!id || id <= 0 || id > results) {
-        return res.status(401).send('Unauthorized Access.');
+  // Gets the folder specifically by the ID.
+  knex('folders')
+    .where('folder.id', id)
+    .andWhere('folder.user_id', 3) // <-- Should be req.body.userId.
+    .first(result => {
+      if(!result) {
+        return res.status(404).send('Not Found.');
       }
-      // After determining if the ID exists and is valid; find it.
-      return knex('users')
-        .where('users.id', id)
-        .first()
-        .then(result => {
-          res.status(200).send(result);
-        })
-        .catch(err => {
-          next(err);
-        });
+
+      res.status(200).send(result);
     })
     .catch(err => {
       next(err);
     });
 });
 
-// GETs: All folders by a certain user.
-router.get('/users/:id', ev(validations.get), (req, res, next) => {
-  // Shit.
-});
-
-// POST: Creates a new folder. 
+// POST: Creates a new folder.
 router.post('/', ev(validations.post), (req, res, next) => {
-  const { firstName, email, password } = req.body;
+  const { name, parent_folder, is_secure } = req.body;
 
-  let newUser = { firstName, email };
-  bcrypt.hash(password, 10).then(result => {
-    newUser.hashed_password = result;
-    return knex('users')
-      .insert(decamelizeKeys(newUser))
-      .then(result => {
-        delete newUser.hashed_password;
-        res.status(200).send(newUser);
-      });
-  })
-  .catch(err => {
-    next(err);
-  });
+  if(!parent_folder) {
+    parent_folder === null;
+  }
+
+  // Change user_id to req.body.userId when token validation is 100%.
+  let newFolder = { name, user_id: 3, parent_folder, is_secure };
+  knex('folders')
+    .insert(newFolder, ['name'])
+    .then(result => {
+      res.status(200).send('Successfully made a folder!');
+    })
+    .catch(err => {
+      next(err);
+    })
 });
 
 // PATCH w/ ID: Updates a user with the ID.
