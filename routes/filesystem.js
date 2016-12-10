0c43bbd79c17b83709f42c648a3e084d02ba005d
malilasage <malilaclearwater@gmail.com>
2016-12-10 15:12:45 -0700

    object returning as expected

--- routes/filesystem.js
+++ routes/filesystem.js
@@ -42,7 +42,21 @@ function filesystem(folders, notes){
       userStuff.notes.push(notes[i]);
     }
   };
-  //add notes with parent folders to their folders but has to happen after folderNotes
+  //adding childFolder arr and folderNotes arr to each folder obj
+  for(var i = 0; i < folders.length; i++) {
+    folders[i].childFolders = [];
+    folders[i].folderNotes = [];
+  };
+  //adding notes to their respective folders
+  for(var i = 0; i < notes.length; i++) {
+    if(notes[i].parent_folder !== null) {
+      for(var x = 0; x < folders.length; x++) {
+        if(notes[i].parent_folder === folders[x].id) {
+                folders[x].folderNotes.push(notes[i]);
+        }
+      }
+    }
+  };
 
   //adding parent folders
   for(var i = 0; i < folders.length; i++) {
@@ -50,11 +64,7 @@ function filesystem(folders, notes){
       userStuff.folders.push(folders[i]);
     }
   };
-  //adding childFolder arr and folderNotes arr to each folder obj
-  for(var i = 0; i < userStuff.folders.length; i++) {
-    userStuff.folders[i].childFolders = [];
-    userStuff.folders[i].folderNotes = [];
-  }
+
   //inserting child folders
   for(var i = 0; i < folders.length; i++) {
     if(folders[i].parent_folder !== null) {
@@ -66,13 +76,12 @@ function filesystem(folders, notes){
     }
   };
   //add notes with parent folders to their respective folders
-  for(var i = 0; i < notes.length; i++) {
-    if(notes[i].parent_folder !== null) {
-      // console.log('not null');
-
-      // userStuff.folders[i].
-    }
-  }
+  // for(var i = 0; i < notes.length; i++) {
+  //   if(notes[i].parent_folder !== null) {
+  //     // console.log('not null');
+  //     userStuff.folders[i].c
+  //   }
+  // }
   return userStuff;
 };
 
