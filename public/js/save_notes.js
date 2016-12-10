7687af50ce2abbb0ada23175778decde32a8a88c
malilasage <malilaclearwater@gmail.com>
2016-12-09 18:14:24 -0700

    current get request cancels if new request is issued before current one finishes. needs testing

--- public/js/save_notes.js
+++ public/js/save_notes.js
@@ -4,6 +4,8 @@ var noteContent;
 var noteId;
 var name = 'note name';
 
+//post note is triggered when note is created, patch note is created when note is saved, delete note is triggered on trashcan button click, get note is triggered on filesystem click
+
 $(document).ready(function() {
   console.log('ready');
   var $button = $('button');
@@ -44,11 +46,18 @@ function patchNote(name, content) {
   .done(console.log('yayyy'));
 }
 
+var currentRequest = null;
+
 function getNote(id) {
-  $.getJSON('/notes/' + noteId)
-  .done((note) => {
-    // window.location.href = 'test.html/notes/10';
-    simplemde.value(note.content);
-    console.log(note);
-  });
+  if(currentRequest != null) {
+    currentRequest.abort();
+  }
+  else{
+    currentRequest = $.getJSON('/notes/' + noteId)
+    .done((note) => {
+      // window.location.href = 'test.html/notes/10';
+      simplemde.value(note.content);
+      console.log(note);
+    });
+  }
 };
