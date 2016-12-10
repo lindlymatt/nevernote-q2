6e2154adb42095a202db84056b6f2f0c5072b151
Matt Pestridge <mpestridge1986@gmail.com>
2016-12-10 14:28:13 -0700

    re-wrote app.html for different page layout. side nav is now functioning! YES

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,9 @@
 
 $(document).ready(function() {
     console.log("ready!");
-    $(".button-collapse").sideNav({
-        menuWidth: 300, // Default is 240
-        edge: 'left', // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        draggable: true // Choose whether you can drag to open on touch screens
-      }
-    );
-
-      // // Show sideNav
-      // $('.button-collapse').sideNav('show');
-      // // Hide sideNav
-      // $('.button-collapse').sideNav('hide');
-      // // Destroy sideNav
-      // $('.button-collapse').sideNav('destroy');
+      // Initialize collapse button
+    $(".button-collapse").sideNav();
+      // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    $('.collapsible').collapsible();
+    $('.dropdown-button').dropdown();
 });
