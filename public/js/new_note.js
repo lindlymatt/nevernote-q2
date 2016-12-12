2aaba92838d4fe36e8c0e2586fb94be3e8f8a15b
malilasage <malilaclearwater@gmail.com>
2016-12-12 11:08:53 -0700

    added functionality to get parent id. added post for new folders/notes. untested

--- public/js/new_note.js
+++ public/js/new_note.js
@@ -1,16 +1,45 @@
 'use strict';
 
 $(document).ready(function() {
-  //on folder form submit post folder()
-  //on note form submit post note()
-
+  var $submit = $('#modal-submit-button');
+  $submit.on('click', function() {
+    var $formType = $('#form-description').text();
+    var $name = $('#form-text').val();
+    var $parent;
+    getParent();
+    if($formType === 'Folder Name: '){
+      getParent('folder');
+      return postFolder($name, $parent);
+    }
+    else if($formType === 'Note Name: '){
+      var noteContent = '';
+      getParent('note');
+      return postNote($name, noteContent, $parent);
+    }
+  });
+  function getParent(type) {
+    var $workspace = $('#workspace').children();
+    if($workspace.hasClass('inside')){
+      var $inside = $('.inside');
+      if($inside.has('.folder')){
+        return $parent = $inside.attr('id').slice(6);
+      }
+      else if($inside.has('.note')){
+        //go to parent. do same as above ^
+        return $parent = $inside.parent().attr('id').slice(4);
+      }
+    }
+    else{
+      return $parent = null;
+    }
+  };
 });
 
 //post new note
-function postNote(name, content, id) {
+function postNote(name, content, parentId) {
   const request = {
     contentType: 'application/JSON',
-    data: JSON.stringify({name, content}),
+    data: JSON.stringify({name, content, parentId}),
     dataType: 'json',
     type: 'POST',
     url: '/notes'
@@ -21,9 +50,15 @@ function postNote(name, content, id) {
 };
 
 //post new folder
-function postFolder(name,  id) {
+function postFolder(name, parentId) {
   const request = {
     contentType: 'application/JSON',
-    data: JSON.stringify({name})
+    data: JSON.stringify({name, parentId}),
+    dataType: 'json',
+    type: 'POST',
+    url: '/folders'
   }
-}
+  console.log(request);
+  $.ajax(request)
+  .done(console.log('done!'));
+};
