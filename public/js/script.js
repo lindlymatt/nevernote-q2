fdc502e41a9aee7006de787492508f09c4cc6296
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-12 00:02:53 -0700

    Added in modal handling, and fucked up the folders on the left.

--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,34 @@
 $(document).ready(function() {
     console.log("ready!");
       // Initialize collapse button
-    $(".button-collapse").sideNav();
+    // $(".button-collapse").sideNav();
       // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    $('.collapsible').collapsible();
-    $('.dropdown-button').dropdown();
+    // $('.collapsible').collapsible();
+    // $('.dropdown-button').dropdown();
+    $('#new-folder').on('click', () => {
+        $('#modal-title').text('Create New Folder');
+        $('#new-icon').attr('src', '../img/new-folder-icon.png');
+        $('#icon-text').text('Create New RE:Folder');
+        $('#icon-description').text('Enter your new folder name, and then hit the submit button to create it!');
+        $('#form-description').text('Folder Name: ');
+        $('#faded-background').show();
+    });
+
+    $('#new-note').on('click', () => {
+        $('#modal-title').text('Create New Note');
+        $('#new-icon').attr('src', '../img/new-note-icon.png');
+        $('#icon-text').text('Create New RE:Note');
+        $('#icon-description').text('Enter your new note name, and then hit the submit button to create it!');
+        $('#form-description').text('Note Name: ');
+        $('#faded-background').show();
+    });
+
+    $('#modal-submit-button').on('click', () => {
+        $('#faded-background').hide();
+    });
+
+    $('#workspace').click(event => {
+        $(event.target).css('background-color', 'white');
+        $(event.target).css('color', 'black');
+    });
 });
