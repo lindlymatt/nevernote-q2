a1729e3f800987afd3e756d745c553a94463f296
Matt Pestridge <mpestridge1986@gmail.com>
2016-12-12 16:26:29 -0700

    dropdown lists open on click and close after mouseleave, though multiple dropdowns can be open at the same time

--- public/js/script.js
+++ public/js/script.js
@@ -69,34 +69,27 @@ $(document).ready(function() {
     let helpDropdown = $("#help-dropdown");
     let sortDropdown = $("#sort-dropdown");
 
-    $('#user-text').click((event) => {
+    $('#user-text').click(() => {
       emailDropdown.toggleClass('show');
     });
 
-    $('#help-text').click((event) => {
+    emailDropdown.mouseleave(() => {
+      emailDropdown.toggleClass('show');
+    });
+
+    $('#help-text').click(() => {
       helpDropdown.toggleClass('show');
     });
 
-    $('#sort-text').click((event) => {
+    helpDropdown.mouseleave(() => {
+      helpDropdown.toggleClass('show');
+    });
+
+    $('#sort-text').click(() => {
       sortDropdown.toggleClass('show');
     });
-    /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//     $("#sort-dropdown").toggleClass("show");
-// }
 
-// Close the dropdown menu if the user clicks outside of it
-// $(window).click = function(event) {
-//   if ($(!event.target).matches($('.dropa'))) {
-//
-//     var dropdowns = $(".dropdown-content");
-//     for (var i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// };
+    sortDropdown.mouseleave(() => {
+      sortDropdown.toggleClass('show');
+    });
 });
