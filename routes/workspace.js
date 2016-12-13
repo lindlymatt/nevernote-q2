0bff49e5548a76cf3892c6b1c0f1d2f9970915bb
Aidan Baack <aebaack@gmail.com>
2016-12-13 13:06:21 -0700

    Add workspace notes for nested notes

--- routes/workspace.js
+++ routes/workspace.js
@@ -21,6 +21,7 @@ router.get('/', (req, res, next) => {
 
       folders.map(folder => {
         folder.childFolders = [];
+        folder.folderNotes = [];
         return folder;
       });
 
@@ -29,7 +30,7 @@ router.get('/', (req, res, next) => {
         if (folder.parent_folder) {
 
           let parentFolder;
-          for (var j = 0; j < folders.length; j++) {
+          for (let j = 0; j < folders.length; j++) {
             let foundParent = findParentFolder(folders[j], folder.parent_folder);
             if (foundParent) {
               parentFolder = foundParent;
@@ -41,7 +42,29 @@ router.get('/', (req, res, next) => {
           i = 0;
         }
       }
-      res.json(folders);
+
+      knex('notes')
+        .innerJoin('user_notes', 'notes.id', 'user_notes.note_id')
+        .where('user_notes.user_id', user)
+        .then((notes) => {
+
+          notes.forEach((note) => {
+            let parentFolder;
+            for (let i = 0; i < folders.length; i++) {
+              let foundParent = findParentFolder(folders[i], note.parent_folder);
+              if (foundParent) {
+                parentFolder = foundParent;
+                break;
+              }
+            }
+            parentFolder.folderNotes.push(note);
+          });
+          
+          res.json(folders);
+        })
+        .catch((err) => {
+          next(err);
+        });
     })
     .catch((err) => {
       next(err);
@@ -67,81 +90,4 @@ function isParent(folder, parentFolderId) {
   return folder.id === parentFolderId;
 }
 
-//
-// router.get('/', (req, res, next) => {
-//   var userFolders;
-//   var userNotes;
-//   console.log(req.body);
-//   Promise.all([
-//     knex('folders')
-//     .where('user_id', req.body.userId)
-//     .orderBy('name')
-//     .then((folders) => {
-//       userFolders = folders;
-//     }),
-//     knex('notes')
-//     .join('user_notes', 'notes.id', '=', 'user_notes.note_id')
-//     .where('user_id', req.body.userId)
-//     .select('*')
-//     .orderBy('name')
-//     .then((notes) => {
-//       userNotes = notes;
-//     })
-//   ]).then(() => {
-//     var result = getWorkspace(userFolders, userNotes);
-//     res.send(camelizeKeys(result));
-//   })
-// });
-//
-// function getWorkspace(folders, notes){
-// //add delete to for loops to reduce runtime
-//   var userStuff = {
-//     folders: [],
-//     notes: [],
-//   };
-//
-//   //adding notes without parents to userStuff & deleting note content 4 security
-//   for(var i = 0; i < notes.length; i++) {
-//     if(notes[i].parent_folder === null) {
-//       userStuff.notes.push(notes[i]);
-//     }
-//     delete notes[i].content;
-//   };
-//
-//   //adding childFolder arr and folderNotes arr to each folder obj
-//   for(var i = 0; i < folders.length; i++) {
-//     folders[i].childFolders = [];
-//     folders[i].folderNotes = [];
-//   };
-//
-//   //adding notes to their respective folders
-//   for(var i = 0; i < notes.length; i++) {
-//     if(notes[i].parent_folder !== null) {
-//       for(var x = 0; x < folders.length; x++) {
-//         if(notes[i].parent_folder === folders[x].id) {
-//                 folders[x].folderNotes.push(notes[i]);
-//         }
-//       }
-//     }
-//   };
-//
-//   //adding parent folders
-//   for(var i = 0; i < folders.length; i++) {
-//     if(folders[i].parent_folder === null) {
-//       userStuff.folders.push(folders[i]);
-//       folders.splice(i, 1);
-//     }
-//   };
-//
-//   //inserting child folders
-//     for(var i = 0; i < folders.length; i++) {
-//         for(var x = 0; x < userStuff.folders.length; x++) {
-//           if(folders[i].parent_folder === userStuff.folders[x].id) {
-//             userStuff.folders[x].childFolders.push(folders[i]);
-//           }
-//         }
-//       }
-//   return userStuff;
-// };
-//
 module.exports = router;
