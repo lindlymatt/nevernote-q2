16ce4f216bccfecb15dda69ce7248f0b1f3b243d
Aidan Baack <aebaack@gmail.com>
2016-12-14 13:00:51 -0700

    Add ability to drag closed folders and maintain note click events

--- public/js/script.js
+++ public/js/script.js
@@ -313,6 +313,33 @@ function dropElement(event) {
   $element.css('display', 'none');
 
   const oldElementId = $element.find('h5').attr('id');
+
+  const noteId = oldElementId.split('_')[1];
+
+  let childElements = $element.find('h5');
+
+  $element.find('h5').each((index, element) => {
+    if ($(element).attr('id').includes('note')) {
+      let noteId = $(element).attr('id').split('_')[1];
+      $(element).on('click', function(event) {
+        clearInterval(window.interval);
+        simplemde.value("Loading...");
+        $.get(`/notes/${noteId}`, data => {
+          clearInterval(window.interval);
+          simplemde.value(data.content);
+          let noteId = data.id;
+          let $current = $('*').find('.inside');
+          $.get(`/notes/${noteId}`, data => {
+            simplemde.value(data.content);
+            interval = setInterval(function() {
+              patchNote(simplemde.value(), noteId);
+            }, 2000);
+          });
+        });
+      });
+    }
+  });
+
   $(`#${oldElementId}`).remove();
 
   $(event.target).parent().append($element);
