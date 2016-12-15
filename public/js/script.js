dd37fbaa0250fde34aece5c192c8d096ca6bb951
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-14 18:44:25 -0700

    I created a modal.

--- public/js/script.js
+++ public/js/script.js
@@ -142,11 +142,15 @@ $(document).ready(function() {
             }
             currentItem.parent().empty();
             currentItem.parent().remove();
+            showModal('success deleted');
             $('#faded-background').hide();
             $('#iframe').hide();
             deleteItem(type, id);
         }
         if($('#modal-title').text().includes('Edit')) {
+            if($('#form-text').val().length > 20) {
+                return showModal('failed edit');
+            }
             let id = 0;
             let type = '';
             let currentItem = $('*').find('.inside');
@@ -161,6 +165,7 @@ $(document).ready(function() {
                 type = 'note';
             }
             name = $('#form-text').val();
+            showModal('success edited');
             $('#faded-background').hide();
             $('#iframe').hide();
             patchName(type, name, id);
@@ -368,4 +373,34 @@ function dropElement(event) {
 function dragOver(event) {
   event.preventDefault();
   event.dataTransfer.dropEffect = 'move';
+}
+
+function showModal(action) {
+    console.log(action);
+  if(action.includes('failed')) {
+    if(action.includes('note')) {
+        $('#toast-text').html('Failed to make note.<br /> Name too long. (20 char max)');
+    }
+    if(action.includes('folder')) {
+        $('#toast-text').html('Failed to make folder.<br /> Name too long. (20 char max)');
+    }
+    if(action.includes('edit')) {
+        $('#toast-text').html('Failed to edit item.<br /> Name too long. (20 char max)');
+    }
+  }
+  if(action.includes('success')) {
+    if(action.includes('deleted')) {
+        $('#toast-text').html('Success! Your file<br /> has been deleted.');
+    }
+    if(action.includes('edited')) {
+        $('#toast-text').html('Success! The name<br /> has been edited.');
+    }
+    if(action.includes('note')) {
+        $('#toast-text').html('Success! Your note<br /> has been created!');
+    }
+    if(action.includes('folder')) {
+        $('#toast-text').html('Success! Your folder<br />has been created!');
+    }
+  }
+  $('#toast').fadeIn(200).delay(1500).fadeOut(200);
 }
